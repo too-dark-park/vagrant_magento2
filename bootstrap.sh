8dd173ffe111e4aa229ac5f1c84b6105cab24503
Scree <sarah.giblette@gmail.com>
2019-08-26 22:12:57 +0100

    Added ext-dom extension (php-xml)

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,12 @@
 
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y php7.2 php7.2-zip composer
+apt-get install -y php7.2 php7.2-zip php-xml composer
+
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 
 apt-get -y update
 apt-get -y install nginx
