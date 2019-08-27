3f5ff7c3fac374de3e9d51a8e9a6c3b9d80696cc
Scree <sarah.giblette@gmail.com>
2019-08-27 08:58:03 +0100

    Added additional extensions required for Magento 2 installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,13 @@
 
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y php7.2 php7.2-zip php-xml composer
+apt-get install -y php7.2 php7.2-xml php7.2-bcmath php7.2-curl php7.2-xmlwriter zip unzip php7.2-zip php7.2-gd php7.2-mysql php7.2-mbstring php7.2-intl php7.2-common php7.2-fpm php7.2-bz2 php7.2-dev
+
+apt-get update
+
+sudo apt-get install -y php7.2-cli curl > /dev/null
+curl -Ss https://getcomposer.org/installer | php > /dev/null
+sudo mv composer.phar /usr/bin/composer
 
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
