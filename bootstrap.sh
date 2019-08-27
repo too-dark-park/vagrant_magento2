cdc773dd6062a0069b31b7ebf0bdfae8d0eccf61
Scree <sarah.giblette@gmail.com>
2019-08-27 09:07:54 +0100

    Added SOAP extension

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y php7.2 php7.2-xml php7.2-bcmath php7.2-curl php7.2-xmlwriter zip unzip php7.2-zip php7.2-gd php7.2-mysql php7.2-mbstring php7.2-intl php7.2-common php7.2-fpm php7.2-bz2 php7.2-dev
+apt-get install -y php7.2 php7.2-xml php7.2-bcmath php7.2-curl php7.2-xmlwriter zip unzip php7.2-zip php7.2-gd php7.2-mysql php7.2-mbstring php7.2-intl php7.2-common php7.2-fpm php7.2-bz2 php7.2-dev php7.2-soap 
 
 apt-get update
 
